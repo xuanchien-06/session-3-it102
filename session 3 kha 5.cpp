10320bb352023b76e9110a796411c36679c903d8
Tạ Xuân Chiến; HN-JPN-KS24C; lập trình scratch; session 3 <xuanchien319@gmail.com>
2024-11-22 06:49:43 +0700

    session 3 khá 5

---
+++ session 3 kha 5.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	printf("                                  danh sach sinh vien                                  \n");
+	printf("|--------------------------------------------------------------------------------------|\n");
+	printf("| STT | Ho va ten       | tuoi | so dien thoai     | email                             |\n");
+	printf("| 1   | Nguyen Van A    | 20   | 0904488481        | dghffuy@gmail.com                 |\n");
+	printf("| 2   | Nguyen Van B    | 21   | 0904488482        | dsjgfgyw@gmail.com                |\n");
+	printf("| 3   | Nguyen Van C    | 18   | 0904488483        | yuergyu@gmail.com                 |\n");
+	printf("| 4   | Nguyen Van D    | 19   | 0904488484        | kjasfghagfhgh@gmail.com           |\n");
+	printf("| 5   | Nguyen Van E    | 22   | 0904488485        | env@rikkeiacademy.com             |\n");
+	printf("| 6   | Nguyen Van F    | 21   | 0904488486        | fnv@rikkeiacademy.com             |\n");
+	printf("| 7   | Nguyen Van G    | 21   | 0904488487        | gnv@rikkeiacademy.com             |\n");
+	printf("| 8   | Nguyen Van H    | 23   | 0904488488        | hnv@rikkeiacademy.com             |\n");
+	printf("| 9   | Nguyen Van I    | 19   | 0904488489        | inv@rikkeiacademy.com             |\n");
+	printf("| 10  | Nguyen Van K    | 25   | 0904488490        | knv@rikkeiacademy.com             |\n");
+	return 0;
+	
+}
