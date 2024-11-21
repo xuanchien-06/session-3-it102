39b3a25ed12620ab0a2022db79c28fbf48e5213c
Tạ Xuân Chiến; HN-JPN-KS24C; lập trình scratch; session 3 <xuanchien319@gmail.com>
2024-11-21 16:15:16 +0700

    session 3 cơ bản 2

---
+++ session 3 coban 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    float nhietdoc;
+	float nhietdof;
+	printf("moi ban nhap nhiet do c ");
+	scanf("%f",&nhietdoc);
+	nhietdof = (1.8*nhietdoc)+32;
+	printf("nhiet do f la %.2fF",nhietdof);
+	return 0;
+}
+	
