7027afe141830f01c97b317c45afa481bc0a6504
Tạ Xuân Chiến; HN-JPN-KS24C; lập trình scratch; session 3 <xuanchien319@gmail.com>
2024-11-22 20:36:21 +0700

    session 3 giỏi 7

---
+++ session 3 gioi 7.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+	//nhap so nguyen co 4 chu so. tinh tong cac chu so va in ra man hinh,in ra dao nguoc
+	//buoc 1: khai bao bien number,sum,reverse,thousand,hundred,tem,unit
+	int number , sum , reverse, thousand, hundred, ten,unit;
+	//buoc 2 :nhap gia tri cho bien number	
+    printf("nhap so nguyen co 4 chu so: ");
+	scanf("%d",&number);
+	//buoc 3: xu li tinh toan de luu gia tri cho cac bien thousand,hundred,tem,unit
+    thousand = number/1000;
+    hundred = (number/100)%10;
+    ten = (number/10)%10;
+    unit = number % 10;
+	//buoc 4:xu li tinh toan sum va reverse
+	sum = thousand+hundred+ten+unit;
+	//buoc 5:in ke qua ra man hinh
+	printf("so nguoi dung nhap vao la: %d \n", number );
+	printf("tong cac chu so cua so %d la: %d\n", number, sum);
+    //buoc 6 ket thuc.
+	return 0;
+}
